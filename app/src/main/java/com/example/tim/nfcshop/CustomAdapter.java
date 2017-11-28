f568582a6a29f3d1194357606b5e3115b2d39a4e
Vladino <qwe.mucha@gmail.com>
2017-11-28 15:50:50 +0100

    Zmena z ListView na RecycledView ktory pouzil na cviku

--- app/src/main/java/com/example/tim/nfcshop/CustomAdapter.java
+++ app/src/main/java/com/example/tim/nfcshop/CustomAdapter.java
@@ -1,34 +1,65 @@
 package com.example.tim.nfcshop;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
 
-public class CustomAdapter extends ArrayAdapter<String> {
 
-    public CustomAdapter(Context context, String[] foods) {
-        super(context, R.layout.custom_row, foods);
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+    private List<String> data;
+
+    public CustomAdapter(List<String> data) {
+        this.data = data;
     }
 
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        View customView = inflater.inflate(R.layout.custom_row, parent, false);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.custom_row,parent,false);
+        return new ViewHolder(v);
+    }
 
-        String foodItem = getItem(position);
-        TextView foodName = (TextView) customView.findViewById(R.id.foodName);
-        TextView foodPrice = (TextView) customView.findViewById(R.id.foodPrice);
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.foodName.setText(data.get(position));
+    }
 
-        foodName.setText(foodItem);
-        foodPrice.setText(Integer.toString(foodItem.length()));//todo: add food price
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
 
-        return customView;
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView foodName;
+        private TextView foodPrice;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            foodName = itemView.findViewById(R.id.foodName);
+            foodName.setOnClickListener(selectSender);
+            foodPrice = itemView.findViewById(R.id.foodPrice);
+            foodPrice.setOnClickListener(deleteSender);
+        }
+
+        private View.OnClickListener selectSender = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        private View.OnClickListener deleteSender = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                data.remove(getLayoutPosition());
+                notifyItemRemoved(getLayoutPosition());
+            }
+        };
     }
 }
--- app/src/main/java/com/example/tim/nfcshop/ShoppingCart.java
+++ app/src/main/java/com/example/tim/nfcshop/ShoppingCart.java
@@ -2,6 +2,8 @@ package com.example.tim.nfcshop;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
@@ -9,6 +11,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -21,19 +24,15 @@ public class ShoppingCart extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String[] foods ={"Káva", "Cappuchino", "Hot-dog", "Minerálna voda", "Bageta"};
-        ListAdapter adapter = new CustomAdapter(getApplicationContext(),foods);
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        List<String> foods = new LinkedList<>();
 
-        listView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        String food = String.valueOf(adapterView.getItemAtPosition(i));//todo: adding items to shopping cart
-                        Toast.makeText(getApplicationContext(), food, Toast.LENGTH_LONG).show();
-                    }
-                }
-        );
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        CustomAdapter adapter = new CustomAdapter(foods);
+        recyclerView.setAdapter(adapter);
     }
 }
